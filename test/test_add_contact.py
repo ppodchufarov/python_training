9bc588dfbed3f826b4673d795df37f15e3bfcebd
PPodchufarov <ppodchufarov@t-argos.ru>
2022-09-27 16:47:02 +0300

    Повышение "интеллектуальности" фикстуры: валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.make(Contact(firstname="qwertyu", home_number="1234567890"))
-    app.session.logout()
+
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.make(Contact(firstname="", home_number=""))
-    app.session.logout()
+
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,12 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="New contact"))
-    app.session.logout()
 
 
 def test_modify_contact_home_number(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(home_number="8887888"))
-    app.session.logout()
