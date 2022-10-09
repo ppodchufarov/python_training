05b59d6e5ccae3d656945e2c2edc55d2212b2c2b
ppodchufarov <ppodchufarov@t-argos.ru>
2022-10-09 13:35:48 +0300

    Повышение "интеллектуальности" теста: обеспечение выполнения предусловий при удалении групп контактов

--- fixture/group.py
+++ fixture/group.py
@@ -65,4 +65,4 @@ class GroupHelper:
     def count(self):
         wd = self.app.wd
         self.open_groups_page()
-        return len(wd.find_elements_by_name("selected[]"))
+        return len(wd.find_element_by_name("selected[]"))
