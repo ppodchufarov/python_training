f803fab213a35ac9d45aea54914989b7b4c866c2
ppodchufarov <ppodchufarov@t-argos.ru>
2022-10-09 14:38:11 +0300

    Повышение "интеллектуальности" теста: обеспечение выполнения предусловий при удалении и модификаций контактов и групп контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,6 @@
-
+from model.contact import Contact
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.make(Contact(firstname="test", home_number="1234567890"))
     app.contact.delete_first_contact()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,8 +2,12 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.make(Contact(firstname="test", home_number="1234567890"))
     app.contact.modify_first_contact(Contact(firstname="New contact"))
 
 
 def test_modify_contact_home_number(app):
+    if app.contact.count() == 0:
+        app.contact.make(Contact(firstname="test", home_number="1234567890"))
     app.contact.modify_first_contact(Contact(home_number="8887888"))
